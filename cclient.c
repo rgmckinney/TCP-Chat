e12445130e5c67c03ba119d4cc627c8e38edf352
Ryan McKinney <ryangmckinney@hotmail.com>
2017-02-17 23:01:21 -0800

    formatting

--- cclient.c
+++ cclient.c
@@ -427,11 +427,11 @@ int setupClient(char *host_name, char *port) {
 }
 
 void printHelp() {
-   printf("\n Message one client:\n      %%M destination-handle {message}\n");
-   printf(" Message multiple clients:\n      %%M num-handles dest-handle-1 dest-handle-2 ... {message}\n");
-   printf("Broadcast a message:\n	%%B {message}\n");
-   printf("List clients in chat\n	%%L\n");
-   printf("	Exit:\n		%%E\n");
+   printf("\n   Message one client:\n      %%M destination-handle {message}\n");
+   printf("   Message multiple clients:\n      %%M num-handles dest-handle-1 dest-handle-2 ... {message}\n");
+   printf("   Broadcast a message:\n      %%B {message}\n");
+   printf("   List clients in chat\n      %%L\n");
+   printf("   Exit:\n      %%E\n");
 }
 
 int main(int argc, char * argv[]) {
